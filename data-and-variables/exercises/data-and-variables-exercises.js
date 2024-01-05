652d9ed53d1b4f8ccebe8073a2e9b232515f8880
Lacie Bradley <lacie.bradley05@gmail.com>
2024-01-05 14:20:45 -0600

    Exercise1

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -5,11 +5,11 @@ let distanceToMarsKm = 225000000;
 let distanceToMoonKm = 38400;
 cons milesPerKm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-console.log typeof (String)
-console.log typeof (Number)
-console.log typeof (Number)
-console.log typeof (Number)
-console.log typeof(Number)
+console.log (typeof "Determination")
+console.log (typeof 17500)
+console.log (typeof 225000000)
+console.log (typeof 38400)
+console.log (typeof 0.621)
 // Calculate a space mission below
 console.log (225000000 * 0.621)
 // Print the results of the space mission calculations below
