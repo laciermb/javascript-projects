169c0489fb27e203d6d086d09854c6a77f559d5b
Lacie Bradley <lacie.bradley05@gmail.com>
2024-01-07 12:18:18 -0600

    Ex1

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -4,7 +4,7 @@ let shuttpleSpeedMph = 17500;
 let distanceToMarsKm = 225000000;
 let distanceToMoonKm = 38400;
 cons milesPerKm = 0.621;
-// Use console.log to print the 'typeof' each variable. Print one item per line.
+
 console.log (typeof "Determination")
 console.log (typeof 17500)
 console.log (typeof 225000000)
@@ -12,8 +12,14 @@ console.log (typeof 38400)
 console.log (typeof 0.621)
 // Calculate a space mission below
 console.log (225000000 * 0.621)
+let milesToMars = kilometersToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttpleSpeedMph
+let daysToMars = hoursToMars / 24
 // Print the results of the space mission calculations below
-
+console.log(shuttleName + "will take" + daysToMars + "days to reach Mars.")
 // Calculate a trip to the moon below
-
+let milesToMoon = kilometersToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.")
